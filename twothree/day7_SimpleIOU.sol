604b5f49829e575213988f5b3c6a6e763e954e76
two-three311 <1817280293@qq.com>
2026-03-08 15:06:20 +0000

    day7_SimpleIOU

--- twothree/day7_SimpleIOU.sol
+++ twothree/day7_SimpleIOU.sol
@@ -6,7 +6,6 @@ contract SimpleIOU {
     mapping(address => bool) public registeredFriends;
     address[] public friendList;
     mapping(address => uint256) public balances;
-
     mapping(address => mapping(address => uint256)) public debts;
 
     constructor() {
@@ -34,12 +33,16 @@ contract SimpleIOU {
     }
 
     function recordDebt(address _creditor, uint256 _amount) public onlyRegistered {
+        require(registeredFriends[_creditor], "Creditor is not registered");
         require(balances[msg.sender] >= _amount,"Insufficient balance");
+        require(_amount > 0, "Amount must be greater than 0");
+        
         balances[msg.sender] -= _amount;
         (bool success, ) = payable(_creditor).call{value: _amount}("");
-    require(success, "Transfer failed");
-    
+        require(success, "Transfer failed");
+        debts[msg.sender][_creditor] += _amount;
     }
+    
 
     function transeferEtherViacall(address payable _to, uint256 _amount) public onlyRegistered {
         require(balances[msg.sender] > _amount, "Insufficient balance");
@@ -56,4 +59,28 @@ contract SimpleIOU {
         (bool success, ) = payable(msg.sender).call{value: _amount}("");
         require(success, "Trancfer failed");
     }
+
+     function registerCreditor(address creditorAddress) public {
+        require(creditorAddress != address(0), "Creditor address cannot be zero");
+        require(!registeredFriends[creditorAddress], "Creditor is already registered");
+        
+        registeredFriends[creditorAddress] = true;
+        friendList.push(creditorAddress);
+    }
+
+
+
+
+    function payFromWallet(address payable _recipient, uint256 _amount) public onlyRegistered {
+        require(_amount > 0, "Amount must be greater than 0");
+        require(registeredFriends[_recipient], "Recipient is not registered");
+        require(balances[msg.sender] >= _amount, "Insufficient wallet balance");
+
+        balances[msg.sender] -= _amount;
+
+        (bool success, ) = _recipient.call{value: _amount}("");
+        require(success, "Payment transfer failed");
+    }
+
+
 }
